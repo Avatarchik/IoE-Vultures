d21ca089b39678a4f0bfee8e50bb18d2875101ef
Eric Johnsen <j0hns3n@Erics-MacBook-Pro-2.local>
2015-11-14 21:29:35 -0600

    Added Unity project
    Signed-off-by: Eric Johnsen <j0hns3n@Erics-MacBook-Pro-2.local>

---
+++ demoProject/Assets/Cardboard/DemoScene/HackButtons.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System;
+using UnityEngine.UI;
+
+//[RequireComponent(typeof(Collider))]
+public class HackButtons : MonoBehaviour {
+
+	public void SetGazedAt(bool gazedAt) {
+		
+		Text textObj = GetComponentInChildren<Text>();
+
+		GameObject answerTextObj = GameObject.Find("AnswerText");
+		Text answerText = answerTextObj.GetComponent<Text>();
+
+		if (String.Equals(textObj.text, "Answer A")) {
+			answerText.text = "A";
+		} else if (String.Equals(textObj.text, "Answer B")) {
+			answerText.text = "B";
+		} else if (String.Equals(textObj.text, "Answer C")) {
+			answerText.text = "C";
+		}
+	}
+
+	public void ResetAnswerText() {
+		GameObject answerTextObj = GameObject.Find("AnswerText");
+		Text answerText = answerTextObj.GetComponent<Text>();
+		answerText.text = "Choose an answer.";
+	}
+
+	public void SelectAnswer() {
+		GameObject answerTextObj = GameObject.Find("AnswerText");
+		Text answerText = answerTextObj.GetComponent<Text>();
+
+		answerText.text = "Something clicked!";
+	}
+}
